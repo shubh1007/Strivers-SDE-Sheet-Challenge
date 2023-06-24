e5e0a30abea44553cb55c876b3409bea3f8f8daf
shubh1007 <b200045@nitsikkim.ac.in>
2023-06-24 16:39:05 +0530

    Kadane's Algorithm Optimized as per Coding Ninjas

--- Arrays/Kadane's Algorihm.py
+++ Arrays/Kadane's Algorihm.py
@@ -10,3 +10,18 @@ class Solution:
             max_sum = max(max_sum, curr_sum)
             curr_sum = max(0, curr_sum)
         return max_sum
+    
+
+# CODING NINJAS
+def maxSubarraySum(arr, n) :
+    curr_sum = 0
+    max_sum = - maxsize - 1
+	# Your code here
+    for i in range(n):
+        curr_sum += arr[i]
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+        if curr_sum < 0:
+            curr_sum = 0
+    # return the answer
+    return max(0, max_sum)
