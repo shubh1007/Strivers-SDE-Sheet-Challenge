ed75cd6570c289b1a3d3d8ad3802514f62c34743
shubh1007 <b200045@nitsikkim.ac.in>
2023-06-25 12:48:50 +0530

    Merge Sort Concepts Cleared

--- Arrays Part-II/Inversion of Array (Pre-req Merge Sort).py
+++ Arrays Part-II/Inversion of Array (Pre-req Merge Sort).py
@@ -1,70 +1,37 @@
-def mergeSort(arr, left, right):
-    if left < right:
-        mid = (left + right) // 2
-        n1 = mid - left + 1
-        n2 = right - mid
-        left_arr = [0 for i in range(n1)]
-        right_arr = [0 for i in range(n2)]
-        temp = [0 for i in range(n1 + n2)]
-        i, j, k = left, mid, 0
-        while i < mid and j <= right:
-            if arr[i] <= arr[j]:
-                
-            
+def merge(arr, low, mid, high):
+    temp = []
+    left = low
+    right = mid + 1
+    count = 0
+    while left <= mid and right <= high:
+        if arr[left] <= arr[right]:
+            temp.append(arr[left])
+            left += 1
+        else:
+            if arr[left] > 2*arr[right]:
+                count += mid - left + 1
+            temp.append(arr[right])
+            right += 1
+    while left <= mid:
+        temp.append(arr[left])
+        left += 1
+    while right <= high:
+        temp.append(arr[right])
+        right += 1
+    for i in range(high - low + 1):
+        arr[low + i] = temp[i]
+    return count
 
+def mergeSort(arr, low, high):
+    count = 0
+    if low < high:
+        mid = (low + high) // 2
+        count += mergeSort(arr, low, mid)
+        count += mergeSort(arr, mid + 1, high)
+        count += merge(arr, low, mid, high)
+    return count
 
-
-
-
-
-arr = [3, 2, 1]
-temp = [0 for i in range(len(arr))] 
-mergeSort(arr, 0, len(arr) - 1)
+arr = [5, 4, 3, 2, 1, 1, 1]
 print(arr)
-
-
-
-
-
-
-# def mergeSort(arr, temp, left, right):
-#     inv_count = 0
-#     if left < right:
-#         mid = (left + right) // 2
-#         inv_count += mergeSort(arr, temp, left, mid)
-#         inv_count += mergeSort(arr, temp, mid + 1, right)
-#         inv_count += merge(arr, temp, left, mid, right)
-#     return inv_count
-
-# def merge(arr, temp, left, mid, right):
-#     i = left
-#     j = mid + 1
-#     k = left
-#     inv_count = 0
-#     while i <= mid and j <= right:
-#         if arr[i] <= arr[j]:
-#             temp[k] = arr[i]
-#             k += 1
-#             i += 1
-#         else:
-#             temp[k] = arr[j]
-#             k += 1
-#             j += 1
-#             inv_count += (mid - i + 1)
-#     while i <= mid:
-#         temp[k] = arr[i]
-#         k += 1
-#         i += 1
-#     while j <= right:
-#         temp[k] = arr[j]
-#         k += 1
-#         j += 1
-#     for loop_var in range(left, right + 1):
-#         arr[loop_var] = temp[loop_var]
-#     return inv_count
-
-# def inversionCount(arr, n):
-#     temp = [0] * n
-#     return mergeSort(arr, temp, 0, n - 1)
-
-
+print(mergeSort(arr, 0, len(arr) - 1))
+print(arr)
